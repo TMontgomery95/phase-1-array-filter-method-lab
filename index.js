32baa68f78a3f161d4f9b9e08bcd19edeff6335b
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-08-11 23:35:39 -0400

    commit tree 4bdf9af36c1493013e331e8afef3414213907585

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, someName) {
+    return drivers.filter (matchingName =>
+        matchingName.toLowerCase() === someName.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, someLetters) {
+    return drivers.filter(matchingName => 
+        matchingName.toLowerCase().indexOf(someLetters.toLowerCase()) === 0    
+    )
+}
+
+function matchName(drivers, exactName) {
+    return drivers.filter(drivers => drivers.name === exactName);  
+}
